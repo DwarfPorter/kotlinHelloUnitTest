e73d5423b209d7bf3fdcc6efe1a3ff86567ed443
DwarfPorter <mrvlmor@gmail.com>
2022-09-24 20:40:00 +0300

    fix test

--- app/src/test/java/ru/geekbrains/simplemvp/ModelUserUnitTest.kt
+++ app/src/test/java/ru/geekbrains/simplemvp/ModelUserUnitTest.kt
@@ -31,6 +31,6 @@ class ModelUserUnitTest {
         val underTest = ModelUser(resources, expected)
         val actual = underTest.name
         assertEquals(expected, actual)
-        assertTrue(false)
+        assertTrue(true)
     }
 }
