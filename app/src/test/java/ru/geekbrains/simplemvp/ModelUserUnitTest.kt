1f7947de0481c9e536cd23805ca2f8ad2e3cd4a1
DwarfPorter <mrvlmor@gmail.com>
2023-12-20 20:33:01 +0300

    fail test

--- app/src/test/java/ru/geekbrains/simplemvp/ModelUserUnitTest.kt
+++ app/src/test/java/ru/geekbrains/simplemvp/ModelUserUnitTest.kt
@@ -31,6 +31,6 @@ class ModelUserUnitTest {
         val underTest = ModelUser(resources, expected)
         val actual = underTest.name
         assertEquals(expected, actual)
-        assertFalse(true)
+        assertTrue(true)
     }
 }
